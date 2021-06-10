1af109ba9fdf3e2b2a9b9fa756594b6890bec82c
Lucas Lepère <llepere@mailgun.com>
2021-06-10 07:58:22 +0200

    Setup the starting branch for exercices

--- App.js
+++ App.js
@@ -1,54 +1,21 @@
-import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import About from './pages/About';
-import SearchScreen from './pages/Search';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Feather } from '@expo/vector-icons';
-import { COLORS } from './styles/Style';
-
-const Tab = createBottomTabNavigator();
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Tab.Navigator
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused }) => {
-                        let iconName;
-
-                        if (route.name === 'A propos') {
-                            iconName = 'user';
-                        } else if (route.name === 'Rechercher') {
-                            iconName = 'home';
-                        }
-
-                        return (
-                            <Feather
-                                name={iconName}
-                                size={30}
-                                color={focused ? COLORS.primary : 'gray'}
-                            />
-                        );
-                    },
-                })}
-                tabBarOptions={{
-                    activeBackgroundColor: '#ffffff',
-                    inactiveBackgroundColor: '#ffffff',
-
-                    activeTintColor: COLORS.primary,
-                    inactiveTintColor: 'gray',
-                    labelStyle: {
-                        fontSize: 16,
-                    },
-                    style: {
-                        height: '7%',
-                    },
-                }}
-            >
-                <Tab.Screen name={'A propos'} component={About} />
-                <Tab.Screen name={'Rechercher'} component={SearchScreen} />
-            </Tab.Navigator>
-        </NavigationContainer>
+      <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <StatusBar style="auto" />
+      </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
